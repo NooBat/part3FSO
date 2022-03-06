fe16f76acbaee1cc0beecf28a08b3b2be6de93c6
Noobat <khoi27012003@gmail.com>
2022-03-07 01:06:13 +0700

    push

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan((tokens, request, response) => {
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
+    JSON.stringify(request.body)
+  ].join(' ')
+}, 'immediate'))
 
 let persons = [
   { 
@@ -57,7 +68,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.post('api/persons', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
   const id = Math.floor(Math.random() * 100)
 
