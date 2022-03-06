8f35c28f9483817e765070e7a47bf89ed2d8268c
Noobat <khoi27012003@gmail.com>
2022-03-06 13:42:09 +0700

    finish exercises 3.1- 3.6

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/info', (request, response) => {
+  response.send(`
+  <div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>
+  </div>  
+  `)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (!person) {
+    response.status(404).end()
+  } else {
+    response.json(person)
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('api/persons', (request, response) => {
+  const body = request.body
+  const id = Math.floor(Math.random() * 100)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "Name or number is missing"
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(403).json({
+      error: "Name must be unique"
+    })
+  }
+
+  const newPerson = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
